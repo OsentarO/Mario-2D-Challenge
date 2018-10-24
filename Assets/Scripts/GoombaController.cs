578f420ab34a72ed2510c333e30ddcbc1c4ef20a
OsentarO <43255445+OsentarO@users.noreply.github.com>
2018-10-24 00:14:59 -0400

    Mario

---
+++ Assets/Scripts/GoombaController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GoombaController : MonoBehaviour {
+
+
+    private Animator anim;
+
+    public float speed;
+
+   // private bool facingRight = false;
+
+    public LayerMask isGround;
+    public LayerMask isPlayer;
+    public Transform wallHitBox;
+    public Transform playerHitBox;
+
+    private bool wallHit;
+    private bool playerhit;
+    public float wallHitHeight;
+    public float wallHitWidth;
+    public float playerHitHeight;
+    public float playerHitWidth;
+
+    private AudioSource source;
+    public AudioClip deathClip;
+    private float volLowRange = .5f;
+    private float volHighRange = 1.0f;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("isDead", false);
+        playerhit = false;
+    }
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+
+        wallHit = Physics2D.OverlapBox(wallHitBox.position, new Vector2(wallHitWidth, wallHitHeight), 0, isGround);
+
+        if (wallHit == true)
+        {
+            speed = speed * -1;
+        }
+
+       /* if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+         {
+             anim.SetBool("isDead", true);
+             speed = 0;
+         }
+         else
+         {
+             transform.Translate(speed * Time.deltaTime, 0, 0);
+             anim.SetBool("IsDead", false);
+
+             wallHit = Physics2D.OverlapBox(wallHitBox.position, new Vector2(wallHitWidth, wallHitHeight), 0, isGround);
+             if (wallHit == true)
+             {
+                 speed = speed * -1;
+             }
+
+             playerhit = Physics2D.OverlapBox(playerHitBox.position, new Vector2(playerHitWidth, playerHitHeight), 0, isPlayer);
+             Debug.Log("playerhit is" + playerhit);
+         }*/
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player" && playerhit == true)
+        {
+            source.PlayOneShot(deathClip);
+            anim.SetBool("isDead", true);
+            //facingRight = !facingRight;
+            //speed = speed * -1;
+            Debug.Log("goomba dead");
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(wallHitBox.position, new Vector3(wallHitWidth, wallHitHeight, 1));
+        Gizmos.color = Color.blue;
+        Gizmos.DrawCube(playerHitBox.position, new Vector3(playerHitWidth, playerHitHeight, 1));
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private bool facingRight = true;
+    private int count;
+    public int numberofCoins;
+   
+
+    public float speed;
+    public float jumpforce;
+    public Text countText;
+    public AudioClip jumpClip;
+    public AudioClip pickupClip;
+
+    private bool isOnGround;
+    public Transform groundcheck;
+    public float checkRadius;
+    public LayerMask allGround;
+
+    //private float jumpTimeCounter;
+    //public float jumpTime;
+    //private bool isJumping;
+
+
+    private AudioSource source;
+
+ 
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        SetCountText();
+    }
+
+
+    void Awake()
+    {
+
+       source = GetComponent<AudioSource>();
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+    void FixedUpdate()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        rb2d.velocity = new Vector2(moveHorizontal * speed, rb2d.velocity.y);
+
+        isOnGround = Physics2D.OverlapCircle(groundcheck.position, checkRadius, allGround);
+
+        Debug.Log(isOnGround);
+
+        if (facingRight == false && moveHorizontal > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && moveHorizontal < 0)
+        {
+            Flip();
+        }
+
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground" && isOnGround)
+
+
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+               
+            {
+                rb2d.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
+                rb2d.velocity = Vector2.up * jumpforce;
+                source.PlayOneShot(jumpClip);
+            }
+        }
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.CompareTag("coin"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 10;
+            SetCountText();
+            source.PlayOneShot(pickupClip);
+        }
+
+        if ((other.gameObject.CompareTag("CoinBox")) && (numberofCoins > 0))
+        {
+            numberofCoins = numberofCoins - 1;
+            count = count + 10;
+            SetCountText();
+            source.PlayOneShot(pickupClip);
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
+
+
+}
